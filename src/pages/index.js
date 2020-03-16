39d00a2d4d78fb3c6a2c1cf34e7081c79d456d09
dcollier <dj@djcollier.com>
2020-03-16 13:04:31 -0400

    Adds more content to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,10 @@ import Layout from '../components/Layout';
 
 export default () => (
   <Layout>
-    <h1>Hello Gatsby!</h1>
+    <h1>Hello! I'm DJ Collier.</h1>
+    <h2>
+      I'm a front-end <i>leaning</i> full stack web developer in the Washington
+      D.C. Metro area.
+    </h2>
   </Layout>
 );
